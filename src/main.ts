adb7d2a8e47c10c951bb74c038ff5606dff3ac1c
ScienceMind11 <sciencemind@icloud.com>
2025-09-02 16:38:57 -0400

    dude i don't even know uhh. icon and some setup stuff like css styles :P

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,18 @@
-import { invoke } from "@tauri-apps/api/core";
+import { TrayIcon } from "@tauri-apps/api/tray";
+import { Menu } from "@tauri-apps/api/menu";
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+const menu = await Menu.new({
+  items: [
+    {
+      id: "quit",
+      text: "Quit"
+    }
+  ]
 });
+
+const options = {
+  menu,
+  menuOnLeftClick: true
+};
+
+const tray = await TrayIcon.new(options);
